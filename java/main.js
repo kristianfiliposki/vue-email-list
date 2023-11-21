8ce4a9cbf59463ae2e27e9752018022ccad9169d
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-21 10:43:12 +0100

    i add button

--- java/main.js
+++ java/main.js
@@ -9,20 +9,31 @@ createApp({
         }
     },
     methods: {
-        email(){           
-        }
-    },
-    mounted() {
-        for (let i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( response=> {
+        genera(){        
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( response=> {
                /* console.log dell'email*/
                 console.log(`ciao ${response.data.response}`)
                 this.mail=response.data.response;
+                if(this.emails.length < 10){
+                    this.emails.push(this.mail);
+                }
+            })       
+        }
+        console.log(this.emails)     }
+    
+    },
+    mounted() {
+        
+        
+/*         for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( response=> {
+                console.log(`ciao ${response.data.response}`)
+                this.mail=response.data.response;
                 this.emails.push(this.mail);
-            })
-            
+            })       
         }
         console.log(this.emails)     
-        
+         */
     }
 }).mount('#app')
